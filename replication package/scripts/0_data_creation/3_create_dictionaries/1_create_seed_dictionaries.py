af543e407395160174a940f4b6687ed10d7a6452
Ulviya Abasova <152338242+UlviyaAbasova@users.noreply.github.com>
2025-06-11 15:04:01 +0200

    Update 1_create_seed_dictionaries.py

--- replication package/scripts/0_data_creation/3_create_dictionaries/1_create_seed_dictionaries.py
+++ replication package/scripts/0_data_creation/3_create_dictionaries/1_create_seed_dictionaries.py
@@ -1,229 +1,190 @@
-# Emotion and Reason in Political LanguageL: Replication Package
+# Emotion and Reason in Political Language: Replication Package
 # Gennaro and Ash
 
 # Description:
-# - Extract lwic dictionaries
-# - Matches them to nlp vocabulary
-# - Eliminate words that are too far
-# Output is the final affect and cognition dictionaries
+# - Extract LIWC dictionaries
+# - Match them to NLP vocabulary
+# - Eliminate words that are too far from centroid
+# Output: final affect and cognition dictionaries and eliminated words
 
 ###################################
 #     Modules                   ###
 ###################################
-
-import string
 import os
+import string
 from glob import glob
 import spacy
 from scipy.spatial import distance
 import pandas as pd
 import numpy as np
 import nltk
-nltk.download('wordnet')
-nlp = spacy.load('en_core_web_lg')  # make sure to use larger model
 from nltk.corpus import wordnet as wn
-import re
 from nltk.tag import perceptron
-tagger = perceptron.PerceptronTagger()
 from nltk.stem.snowball import SnowballStemmer
-stemmer = SnowballStemmer("english")
 import joblib
+import re
+
+# Ensure NLTK resources
+nltk.download('wordnet', quiet=True)
 
 ###################################
-#     Working directories       ###
+#     Fixed Paths               ###
 ###################################
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.normpath(os.path.join(script_dir, '../../../data'))
+liwc_dir = os.path.join(data_dir, 'liwc')
+results_dir = os.path.normpath(os.path.join(script_dir, '../../../results'))
 
-wd_data = './data'
-wd_results = './results'
+# Create directories if missing
+os.makedirs(liwc_dir, exist_ok=True)
+os.makedirs(results_dir, exist_ok=True)
 
-# Set working directory
-os.chdir(wd_data)
+###################################
+#     Load SpaCy Model          ###
+###################################
+try:
+    nlp = spacy.load('en_core_web_lg')
+except OSError:
+    import subprocess
+    subprocess.run(['python3', '-m', 'spacy', 'download', 'en_core_web_sm'], check=True)
+    nlp = spacy.load('en_core_web_sm')
 
+###################################
+#     Initialize NLP tools      ###
+###################################
+tagger = perceptron.PerceptronTagger()
+stemmer = SnowballStemmer('english')
 
 ###################################
 #     Match stems               ###
 ###################################
-
-# Find list and direction of liwc lists
-liwc_files = glob('liwc/*txt')
-
-# Create a dictionnary where the key is the liwc label 
-# and each entry is the list of tokens from liwc
-# Selecting only affect and cognition
-
+liwc_files = glob(os.path.join(liwc_dir, '*.txt'))
 labels2words = {}
 for lfile in liwc_files:
-    label = lfile[:-4].split('-')[-1] # this extracts the label from the title of liwc
-    stems = open(lfile).read().split() # creates tokens from each liwc list
-    labels2words[label] = stems # I give the liwc label as key to the dict and add the words
+    label = os.path.splitext(os.path.basename(lfile))[0].split('-')[-1]
+    tokens = open(lfile, encoding='utf-8').read().split()
+    labels2words[label] = tokens
 
-del lfile
-del label
-del liwc_files
-del stems
+# Focus on affect and cogproc
+mydict = {k: labels2words.get(k, []) for k in ['affect', 'cogproc']}
 
-# I extract only the two keys affect and cogproc and create separate lists for affect and cogproc / wildcards and tokens
-mydict = dict([ (k, labels2words.get(k, None)) for k in ['affect', 'cogproc']])
-
-
-# Separate wildcards from clean tokens for affect
-tokens_affect = set()
-wildcards_affect = ''
-
-for x in mydict['affect']:
-    if '*' in x:
-        x = x.replace('*','[a-z]*') # this replaces * with [a-z]* and puts all the liwc stems in wildcards
-        wildcards_affect += '(' + x + ')|'
-    else:
-        tokens_affect.add(x) # only "clean" tokens get into the tokens set
-wildcards_affect = wildcards_affect[:-1]
-del x
-
-
-# Separate wildcards from clean tokens for cognition
-tokens_cog = set()
-wildcards_cog = ''
-
-for x in mydict['cogproc']:
-    if '*' in x:
-        x = x.replace('*','[a-z]*') # this replaces * with [a-z]* and puts all the liwc stems in wildcards
-        wildcards_cog += '(' + x + ')|'
-    else:
-        tokens_cog.add(x) # only "clean" tokens get into the tokens set
-wildcards_cog = wildcards_cog[:-1]
-del x
-
-
-# Find the wildcards (after pattern match)
-# AFFECT : complete wildcards
-wild_affect_matches = []
-for i, word in enumerate(wn.all_synsets()):
-    w = word.lemma_names()[0].lower()
-    if '_' in w:
-        continue
-    if re.match(wildcards_affect, w): # keeps a word if it matches with the pattern of wildcard above
-        wild_affect_matches.append(w)
- #       print(i,w)
-wild_affect_matches = set(wild_affect_matches)
-del w
-del i
-
-tokens_affect = list(tokens_affect.union(wild_affect_matches))
-tokens_affect = [s for s in tokens_affect if set.intersection(set(string.punctuation),set(list(s)))==set()]
-
-
-# CONGITION : complete wildcards
-wild_cog_matches = []
-
-for i, word in enumerate(wn.all_synsets()):
-    w = word.lemma_names()[0].lower()
-    if '_' in w:
-        continue
-    if re.match(wildcards_cog, w): # keeps a word if it matches with the pattern of wildcard above
-        wild_cog_matches.append(w)
- #       print(i,w)
-wild_cog_matches = set(wild_cog_matches)
-del i
-del w
-
-
-tokens_cog = list(tokens_cog.union(wild_cog_matches))
-tokens_cog = [s for s in tokens_cog if set.intersection(set(string.punctuation),set(list(s)))==set()]
-
-
-# eliminate common words between the two lists
-intersection = sorted(list(set.intersection(set(tokens_cog),set(tokens_affect))))
-tokens_cog = [s for s in tokens_cog if s not in intersection]
-tokens_affect = [s for s in tokens_affect if s not in intersection]
-del intersection
-
-# eliminate words not in nlp vocabulary as these are not assigned any vector
-tokens_cog = [s for s in tokens_cog if s in nlp.vocab]
-tokens_affect = [s for s in tokens_affect if s in nlp.vocab]
-
-
-###################################
-# Eliminate words that are unrelated
-###################################
-
-def findunrelatedwords(tokens):
-    s = ' '.join(tokens) # put initial words in a string
-    s = nlp(s) # find word vectors with pre-trained vectors
-    center = s.vector
-    l= [] # list of tuples (word, cosine distance from the centroid)
-    for t in range(len(tokens)):
-        if s[t].has_vector:
-            d = (s[t].text, distance.cosine(center, s[t].vector))
-            l.append(d)
+# Separate tokens and wildcard patterns
+def split_tokens_wildcards(word_list):
+    tokens, patterns = set(), []
+    for w in word_list:
+        if '*' in w:
+            patterns.append(w.replace('*', '[a-z]*'))
         else:
+            tokens.add(w)
+    return tokens, patterns
+
+tokens_affect, patterns_affect = split_tokens_wildcards(mydict['affect'])
+tokens_cog, patterns_cog = split_tokens_wildcards(mydict['cogproc'])
+
+# Expand patterns via WordNet
+def expand_wildcards(patterns):
+    matches = set()
+    if not patterns:
+        return matches
+    regex = re.compile('(' + ')|('.join(patterns) + ')')
+    for syn in wn.all_synsets():
+        w = syn.lemma_names()[0].lower()
+        if '_' in w:
             continue
-    l = sorted(l, key=lambda x: x[1])
-    df = pd.DataFrame(l)
-    df.columns = ['token', 'distance']
-    df = df.dropna()
-    df = df.loc[df['distance']<np.percentile(df['distance'], 75)] # eliminate the 25% most dissimilar words
-    final = sorted(list(df['token']))
-    return final;
+        if regex.match(w):
+            matches.add(w)
+    return matches
 
-affect = findunrelatedwords(tokens_affect)
-cognition = findunrelatedwords(tokens_cog)
+tokens_affect |= expand_wildcards(patterns_affect)
+tokens_cog |= expand_wildcards(patterns_cog)
 
+# Remove punctuation tokens
+tokens_affect = {w for w in tokens_affect if not set(w) & set(string.punctuation)}
+tokens_cog = {w for w in tokens_cog if not set(w) & set(string.punctuation)}
 
-# List of eliminated words here - for the paper
-c = [a for a in tokens_cog if a not in cognition]
-a = [b for b in tokens_affect if b not in affect]
-os.chdir(results)
-with open ('eliminated_words_affect.txt', 'w') as fo:
-     fo.write(','.join(str(i) for i in a))
-with open ('eliminated_words_cognition.txt', 'w') as fo:
-     fo.write(','.join(str(i) for i in c))
+# Remove intersection
+common = tokens_affect & tokens_cog
+tokens_affect -= common
+tokens_cog -= common
 
-print(len(c)) # Number of cognitive words eliminated with findunrelatedwords
-print(len(a)) # Number of affect words eliminated with findunrelatedwords
-
+# Keep only tokens in spaCy vocab
+tokens_affect = [w for w in tokens_affect if w in nlp.vocab]
+tokens_cog = [w for w in tokens_cog if w in nlp.vocab]
 
 ###################################
-# Keep only adjectives and verbs
+# Eliminate unrelated words     ###
 ###################################
+def find_unrelated(tokens):
+    if not tokens:
+        return []
+    doc = nlp(' '.join(tokens))
+    center = doc.vector
+    dist_list = []
+    for i, token in enumerate(tokens):
+        if doc[i].has_vector:
+            dist_list.append((token, distance.cosine(center, doc[i].vector)))
+    if not dist_list:
+        return []
+    df = pd.DataFrame(dist_list, columns=['token', 'distance'])
+    if df.empty:
+        return []
+    cutoff = np.percentile(df['distance'], 75)
+    return sorted(df.loc[df['distance'] < cutoff, 'token'].tolist())
 
-tagged_affect = tagger.tag(affect)
-tagged_cog = tagger.tag(cognition)
-affect = [i[0] for i in tagged_affect if i[1].startswith(('N', 'V', 'J'))]
-cognition = [i[0] for i in tagged_cog if i[1].startswith(('N', 'V', 'J'))]
+final_affect = find_unrelated(tokens_affect)
+final_cog = find_unrelated(tokens_cog)
 
-del tagged_affect
-del tagged_cog
+# Words eliminated
+elim_affect = set(tokens_affect) - set(final_affect)
+elim_cog = set(tokens_cog) - set(final_cog)
 
-# Stem lwic and eliminate doubles
-cognition = list(set([stemmer.stem(item) for item in cognition]))
-affect = list(set([stemmer.stem(item) for item in affect]))
+###################################
+#     Post-processing           ###
+###################################
+# POS filter and stemming
+tag_aff = tagger.tag(final_affect)
+tag_cog = tagger.tag(final_cog)
+final_affect = {stemmer.stem(w) for w, pos in tag_aff if pos.startswith(('N','V','J'))}
+final_cog = {stemmer.stem(w) for w, pos in tag_cog if pos.startswith(('N','V','J'))}
 
-# eliminate commons
-intersection = sorted(list(set.intersection(set(cognition),set(affect))))
-cognition = [s for s in cognition if s not in intersection]
-affect = [s for s in affect if s not in intersection]
-del intersection
+# Remove stopwords and procedural words
+stop = set()
 
+# Load stopwords if available
+stop_path = os.path.join(data_dir, 'stopwords.pkl')
+if os.path.exists(stop_path):
+    stop = set(joblib.load(stop_path))
+else:
+    print(f"Warning: stopwords file not found at {stop_path}")
 
-# THIS IS THE NEW STEP WRT THE PREVIOUS VERSION
-# eliminate stopwords
-os.chdir(wd_data)
+# Load procedural words if available
+proc_path = os.path.join(data_dir, 'procedural_words.pkl')
+if os.path.exists(proc_path):
+    stop |= set(joblib.load(proc_path))
+else:
+    print(f"Note: procedural words file not found at {proc_path}, skipping")
 
-stop = joblib.load('stopwords/stopwords.pkl')
-proc = joblib.load('procedural language/procedural_words.pkl')
-stop = set(stop).union(proc)
+final_affect -= stop
+final_cog -= stop
 
-affect = [b for b in affect if b not in stop]
-cognition = [b for b in cognition if b not in stop]
+###################################
+#     Save Outputs              ###              ###
+###################################
+dict_dir = os.path.join(results_dir, 'dictionaries')
+os.makedirs(dict_dir, exist_ok=True)
+joblib.dump(list(final_cog), os.path.join(dict_dir, 'dictionary_cognition.pkl'))
+joblib.dump(list(final_affect), os.path.join(dict_dir, 'dictionary_affect.pkl'))
 
+# Text files
+with open(os.path.join(dict_dir, 'dictionary_affect.txt'), 'w') as f:
+    f.write(','.join(final_affect))
+with open(os.path.join(dict_dir, 'dictionary_cognition.txt'), 'w') as f:
+    f.write(','.join(final_cog))
 
-# Save dictionaries for later use
-joblib.dump(cognition, 'dictionaries/dictionary_cognition.pkl')
-joblib.dump(affect,'dictionaries/dictionary_affect.pkl') # save final list
+# Eliminated lists
+with open(os.path.join(results_dir, 'eliminated_words_affect.txt'), 'w') as f:
+    f.write(','.join(elim_affect))
+with open(os.path.join(results_dir, 'eliminated_words_cognition.txt'), 'w') as f:
+    f.write(','.join(elim_cog))
 
-# Save of affect and cog words into txt file
-os.chdir(wd_results)
-with open ('dictionaries/dictionary_affect.txt', 'w') as fo:
-     fo.write(','.join(str(i) for i in affect))
-with open ('dictionaries/dictionary_cognition.txt', 'w') as fo:
-     fo.write(','.join(str(i) for i in cognition))
+print("LIWC dictionaries created and saved.")
